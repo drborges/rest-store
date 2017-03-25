8c1e9d9e3dbc2e6706d975140c17369a777fb4d3
Diego Borges <drborges.cic@gmail.com>
2017-03-25 12:35:27 -0300

    Implement Router#fetch

--- test/reduxful.spec.js
+++ test/reduxful.spec.js
@@ -16,4 +16,41 @@ describe("Router", () => {
       expect(Router.resolve("/users/:index/comments/:index", [0, 1])).to.deep.equal(["users", 0, "comments", 1])
     })
   })
+
+  describe("#fetch", () => {
+    const state = {
+      users: [
+        { name: "diego" },
+        { name: "bibi", comments: [{ text: "LoL" }] }
+      ]
+    }
+
+    it("fetches subtree for path /users", () => {
+      const expectedSubtree = state.users
+      const actualSubtree = Router.fetch(state, Router.resolve("/users"))
+
+      expect(actualSubtree).to.deep.equal(expectedSubtree)
+    })
+
+    it("fetches subtree for path /users/1", () => {
+      const expectedSubtree = state.users[1]
+      const actualSubtree = Router.fetch(state, Router.resolve("/users/:index", [1]))
+
+      expect(actualSubtree).to.deep.equal(expectedSubtree)
+    })
+
+    it("returns undefined when subtree does not exist", () => {
+      const expectedSubtree = undefined
+      const actualSubtree = Router.fetch(state, Router.resolve("/users/:index/comments/:index", [0, 0]))
+
+      expect(actualSubtree).to.be.undefined
+    })
+
+    it("fetches subtree for path /users/1/comments/0", () => {
+      const expectedSubtree = state.users[1].comments[0]
+      const actualSubtree = Router.fetch(state, Router.resolve("/users/:index/comments/:index", [1, 0]))
+
+      expect(actualSubtree).to.deep.equal(expectedSubtree)
+    })
+  })
 })
